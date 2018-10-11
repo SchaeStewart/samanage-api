5659c81abfed2dab7b79453f3eced156f406fc41
Schae Stewart <schaestewart@gmail.com>
2018-10-11 15:49:52 -0400

    Move functions into constructor and change to objects

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const axios = require('axios');
 
 /** Class for interacting with the Samanage API*/
 class Samanage {
-
     /**
      *
      * @param {string} key - Samanage API Key
@@ -12,311 +11,264 @@ class Samanage {
      * @param {string} [contentType=json] - JSON or XML
      */
     constructor(key, version = '2.1', contentType = 'json') {
-        if (key === '' || typeof key !== 'string') throw 'An API Key is required';
+        if (!key || typeof key !== 'string') throw 'An API Key is required';
         axios.defaults.baseURL = 'https://api.samanage.com';
         axios.defaults.headers.common['X-Samanage-Authorization'] = `Bearer ${key}`;
         axios.defaults.headers.common['Accept'] = `application/vnd.samanage.v${version}+${contentType}`;
         axios.defaults.headers.common['Content-Type'] = `application/${contentType}`;
         this.contentType = contentType;
-    }
 
-    /**
-     * Creates a string of query parameters using an objects key/value pairs
-     * @param {object} parameters
-     * @returns {string}
-     */
-    createQueryString(parameters) {
-        return Object.keys(parameters)
-            .map(key => (`${key}=${parameters[key]}`))
-            .reduce((previous, current) => (`${previous}${current}&`), '?')
-            .slice(0, -1); // Removes dangling '&'
-    }
-
-    /**
-     * Gets a specified resource.
-     * @param {string} resourceName - Name of the Samange resource. EX 'hardware' 'users'
-     * @param {object} parameters - Uses the objects' key/value pairs to create query parameters. EX { per_page: 10 } = ?per_page=10
-     * @returns {Promise}
-     */
-    get(resourceName, { id, parameters = {} }) {
-        return axios.get(`/${resourceName}${id ? '/' + id : null}.${this.contentType}${this.createQueryString(parameters)}`);
-    }
-
-    /**
-     * Updates a specified resource
-     * @param {string} resourceName - Name of the Samange resource. EX 'hardware' 'users'
-     * @param {string} id - The id of the resource
-     * @param {object} data - An object containing the fields to be update. EX { user: { email: 'NewEmail@domain.com' } }
-     * @returns {Promise}
-     */
-    update(resourceName, id, data) {
-        return axios.put(`/${resourceName}/${id}.${this.contentType}`, data);
-    }
-
-    /**
-     * Deletes a specified resource
-     * @param {string} resourceName - Name of the Samange resource. EX 'hardware' 'users'
-     * @param id
-     * @param {string} id - The id of the resource
-     * @returns {Promise}
-     */
-    delete(resourceName, id) {
-        return axios.delete(`/${resourceName}/${id}.${this.contentType}`);
-    }
-
-    /**
-     * Creates a specified resource
-     * @param {string} resourceName - Name of the Samange resource. EX 'hardware' 'users'
-     * @param {object} data - An object containing the resources fields. EX { user: { email: 'NewEmail@domain.com' } }
-     * @returns {Promise}
-     */
-    create(resourceName, data) {
-        return axios.post(`${resourceName}.json`, data);
-    }
-
-    logError(msg) {
-        return (error) => {
-            // eslint-disable-next-line no-console
-            console.log(error, msg); 
+        /**
+         * Creates a string of query parameters using an objects key/value pairs
+         * @param {object} parameters
+         * @returns {string}
+         */
+        this.createQueryString = function (parameters) {
+            return Object.keys(parameters)
+                .map(key => (`${key}=${parameters[key]}`))
+                .reduce((previous, current) => (`${previous}${current}&`), '?')
+                .slice(0, -1); // Removes dangling '&'
         };
-    }
 
-    attachments() {
-        return {
+        /**
+         * Gets a specified resource.
+         * @param {string} resourceName - Name of the Samange resource. EX 'hardware' 'users'
+         * @param {object} parameters - Uses the objects' key/value pairs to create query parameters. EX { per_page: 10 } = ?per_page=10
+         * @returns {Promise}
+         */
+        this.get = function (resourceName, parameters = {} ) {
+            const { id } = parameters;
+            if(id) delete parameters.id;
+            return axios.get(`/${resourceName}${id ? '/' + id : ''}.${this.contentType}${this.createQueryString(parameters)}`);
+        }; 
+
+        /**
+         * Updates a specified resource
+         * @param {string} resourceName - Name of the Samange resource. EX 'hardware' 'users'
+         * @param {string} id - The id of the resource
+         * @param {object} data - An object containing the fields to be update. EX { user: { email: 'NewEmail@domain.com' } }
+         * @returns {Promise}
+         */
+        this.update = function(resourceName, id, data) {
+            return axios.put(`/${resourceName}/${id}.${this.contentType}`, data);
+        };
+
+        /**
+         * Deletes a specified resource
+         * @param {string} resourceName - Name of the Samange resource. EX 'hardware' 'users'
+         * @param id
+         * @param {string} id - The id of the resource
+         * @returns {Promise}
+         */
+        this.delete = function (resourceName, id) {
+            return axios.delete(`/${resourceName}/${id}.${this.contentType}`);
+        };
+
+        /**
+         * Creates a specified resource
+         * @param {string} resourceName - Name of the Samange resource. EX 'hardware' 'users'
+         * @param {object} data - An object containing the resources fields. EX { user: { email: 'NewEmail@domain.com' } }
+         * @returns {Promise}
+         */
+        this.create = function (resourceName, data) {
+            return axios.post(`${resourceName}.json`, data);
+        };
+
+        this.logError = function(msg) {
+            return (error) => {
+            // eslint-disable-next-line no-console
+                console.log(error, msg); 
+            };
+        };
+
+
+        this.attachments = {
             create: this.create.bind(this, 'attachments'),
         };
-    }
 
-    audit() {
-        return {
+        this.audit = {
             get: this.get.bind(this, 'audits')
         };
-    }
 
-    catalogItems() {
-        return {
+        this.catalogItems =  {
             create: this.create.bind(this, 'catalog_items'),
             get: this.get.bind(this, 'catalog_items'),
             update: this.update.bind(this, 'catalog_items'),
         };
-    }
 
-    categories () {
-        return {
+        this.categories = {
             create: this.create.bind(this, 'categories'),
             get: this.get.bind(this, 'categories'),
             update: this.update.bind(this, 'categories'),
             delete: this.delete.bind(this, 'categories')
         };
-    }
 
-    changes() {
-        return {
+        this.changes = {
             create: this.create.bind(this, 'changes'),
-            get: this.create.bind(this, 'changes'),
+            get: this.get.bind(this, 'changes'),
             update: this.update.bind(this, 'changes'),
             delete: this.delete.bind(this, 'changes')
         };
-    }
 
-    comments() {
-        throw 'error, feature not implemented';
-    }
+        this.comments = () => {
+            throw 'error, feature not implemented';
+        };
 
-    configurationItems() {
-        return {
+        this.configurationItems = {
             create: this.create.bind(this, 'configuration_items'),
-            get: this.create.bind(this, 'configuration_items'),
+            get: this.get.bind(this, 'configuration_items'),
             update: this.update.bind(this, 'configuration_items'),
             delete: this.delete.bind(this, 'configuration_items')
         };
-    }
 
 
-    contracts() {
-        return {
+        this.contracts = {
             create: this.create.bind(this, 'contracts'),
-            get: this.create.bind(this, 'contracts'),
+            get: this.get.bind(this, 'contracts'),
             update: this.update.bind(this, 'contracts'),
             delete:  () => { throw 'delete is not a function for contracts'; }
         };
-    }
 
-    departments() {
-        return {
+        this.departments = {
             create: this.create.bind(this, 'departments'),
-            get: this.create.bind(this, 'departments'),
+            get: this.get.bind(this, 'departments'),
             update: this.update.bind(this, 'departments'),
             delete: this.delete.bind(this, 'departments')
         };
-    }
 
-    groups() {
-        return {
+        this.groups = {
             create: this.create.bind(this, 'groups'),
-            get: this.create.bind(this, 'groups'),
+            get: this.get.bind(this, 'groups'),
             update: this.update.bind(this, 'groups'),
             delete: this.delete.bind(this, 'groups')
         };
-    }
 
-    hardware() {
-        return {
+        this.hardware = {
             create: this.create.bind(this, 'hardwares'),
-            get: this.create.bind(this, 'hardwares'),
+            get: this.get.bind(this, 'hardwares'),
             update: this.update.bind(this, 'hardwares'),
             delete: this.delete.bind(this, 'hardwares')
         };
-    }
 
-    incidents() {
-        return {
+        this.incidents = {
             create: this.create.bind(this, 'incidents'),
-            get: this.create.bind(this, 'incidents'),
+            get: this.get.bind(this, 'incidents'),
             update: this.update.bind(this, 'incidents'),
             delete: this.delete.bind(this, 'incidents')
         };
-    }
 
-    items() {
-        throw 'error feature not implemented';
-    }
+        this.items = () => {
+            throw 'error feature not implemented';
+        };
 
-    memberships() {
-        return {
+        this.memberships = {
             create: this.create.bind(this, 'memberships'),
-            get: this.create.bind(this, 'memberships'),
+            get: this.get.bind(this, 'memberships'),
             update: this.update.bind(this, 'memberships'),
             delete: this.delete.bind(this, 'memberships')
         };
-    }
 
-    mobiles() {
-        return {
+        this.mobiles = {
             create: this.create.bind(this, 'mobiles'),
-            get: this.create.bind(this, 'mobiles'),
+            get: this.get.bind(this, 'mobiles'),
             update: this.update.bind(this, 'mobiles'),
             delete: this.delete.bind(this, 'mobiles')
         };
-    }
 
-    otherAssets() {
-        return {
+        this.otherAssets = {
             create: this.create.bind(this, 'other_assets'),
-            get: this.create.bind(this, 'other_assets'),
+            get: this.get.bind(this, 'other_assets'),
             update: this.update.bind(this, 'other_assets'),
             delete: this.delete.bind(this, 'other_assets')
         };
-    }
 
-    printers() {
-        return {
-            get: this.create.bind(this, 'printers'),
+        this.printers = {
+            get: this.get.bind(this, 'printers'),
         };
-    }
 
-    problems() {
-        return {
+        this.problems = {
             create: this.create.bind(this, 'problems'),
-            get: this.create.bind(this, 'problems'),
+            get: this.get.bind(this, 'problems'),
             update: this.update.bind(this, 'problems'),
             delete: this.delete.bind(this, 'problems')
         };
-    }
 
-    purchaes() {
-        throw 'error purchaes not implemented';
-    }
+        this.purchae = () => {
+            throw 'error purchaes not implemented';
+        };
 
-    purchaseOrders() {
-        return {
+        this.purchaseOrders = {
             create: this.create.bind(this, 'purchase_orders'),
-            get: this.create.bind(this, 'purchase_orders'),
+            get: this.get.bind(this, 'purchase_orders'),
             update: this.update.bind(this, 'purchase_orders'),
             delete: this.delete.bind(this, 'purchase_orders')
         };
-    }
 
-    releases() {
-        return {
+        this.releases = {
             create: this.create.bind(this, 'releases'),
-            get: this.create.bind(this, 'releases'),
+            get: this.get.bind(this, 'releases'),
             update: this.update.bind(this, 'releases'),
             delete: this.delete.bind(this, 'releases')
         };
-    }
 
-    risks() {
-        return {
-            get: this.create.bind(this, 'risks'),
+        this.risks = {
+            get: this.get.bind(this, 'risks'),
             getForHardware: (hardwareId) => this.get(`hardwares/${hardwareId}/risks.${this.contentType}`)
         };
-    }
 
-    roles() {
-        return {
+        this.roles = {
             create: this.create.bind(this, 'role'),
-            get: this.create.bind(this, 'role'),
+            get: this.get.bind(this, 'role'),
         };
-    }
 
-    serviceRequests() {
-        throw 'error feature not implemented';
-    }
+        this.serviceRequests = () => {
+            throw 'error feature not implemented';
+        };
 
-    sites() {
-        return {
+        this.sites = {
             create: this.create.bind(this, 'sites'),
             get: this.get.bind(this, 'sites'),
             update: this.update.bind(this, 'sites'),
             delete: this.delete.bind(this, 'sites'),
         };
-    }
 
-    software() {
-        return {
+        this.software = {
             get: this.get.bind(this, 'softwares'),
         };
-    }
 
-    solutions() {
-        return {
+        this.solutions = {
             create: this.create.bind(this, 'solutions'),
             get: this.get.bind(this, 'solutions'),
             update: this.update.bind(this, 'solutions'),
             delete: this.delete.bind(this, 'solutions'),
         };
-    }
 
-    tasks() {
-        throw 'error feature not implemented';
-    }
+        this.tasks = () => {
+            throw 'error feature not implemented';
+        };
 
-    timeTracks() {
-        throw 'error feature not implemented';
-    }
+        this.timeTracks = () => {
+            throw 'error feature not implemented';
+        };
 
-    users() {
-        return {
+        this.users = {
             create: this.create.bind(this, 'users'),
             get: this.get.bind(this, 'users'),
             update: this.update.bind(this, 'users'),
             delete: this.delete.bind(this, 'users'),
         };
-    }
 
-    vendor() {
-        return {
+        this.vendor = {
             get: this.get.bind(this, 'vendors')
         };
-    }
 
-    warranties() {
-        throw 'error feature not implemented';
+        this.warranties = () => {
+            throw 'error feature not implemented';
+        };
     }
 }
 
 const api = new Samanage(process.env.SAMANAGE_KEY);
-api.comments().create();
-// .then(res => console.log(res.data))
-// .catch((error) => console.log(error, 'error'));
+api.hardware.get()
+    .then(res => console.log(res.data))
+    .catch((error) => console.log(error, 'error'));
