1d1e5455fa074869fee0e60e2005c4e48f4ad931
Schae Stewart <schaestewart@gmail.com>
2018-10-15 13:40:31 -0400

    add lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,31 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    // "extends": "eslint:recommended",
-    "parser": "typescript-eslint-parser",
-    "plugins": [
-        "typescript"
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: ["prettier"],
+  parser: "typescript-eslint-parser",
+  plugins: ["typescript"],
+  parserOptions: {
+    sourceType: "module"
+  },
+  rules: {
+    indent: ["error", 4],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "typescript/class-name-casing": "error",
+    "typescript/explicit-function-return-type": [
+      "error",
+      { allowExpressions: true }
     ],
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "typescript/class-name-casing": "error",
-        "typescript/explicit-function-return-type": [ "error", { "allowExpressions": true }],
-        "typescript/member-delimiter-style": ["error", {
-            "delimiter": "comma", "requireLast": true
-        }],
-        "typescript/no-unused-vars": "error" 
-    }
+    "typescript/member-delimiter-style": [
+      "error",
+      {
+        delimiter: "comma",
+        requireLast: true
+      }
+    ],
+    "typescript/no-unused-vars": "error"
+  }
 };
